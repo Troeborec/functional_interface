7f0a22b171e3f5f2ae833523c98ddb53dbf56079
functional_interface <defied_jeff@mail.ru>
2023-03-23 14:38:11 +1200

    1 задание

--- src/DogPredicate.java
+++ src/DogPredicate.java
@@ -3,10 +3,10 @@ import java.util.function.Predicate;
 public class DogPredicate implements Predicate<Dog> {
 
 
-    private static final int dogAgePredicate = 10;
+    private static final int dogAgePredicate = 0;
 
     @Override
     public boolean test(Dog dog) {
-        return dog.getAge() <= dogAgePredicate;
+        return dog.getAge() >= dogAgePredicate;
     }
 }
