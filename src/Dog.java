e9baa033bb748f12c1c2b88866335823ca101bce
functional_interface <defied_jeff@mail.ru>
2023-03-23 13:10:59 +1200

    1 задание

---
+++ src/Dog.java
@@ -0,0 +1,18 @@
+public class Dog {
+    private final String name;
+    private final int age;
+
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1");
+        List<Dog> dogList = Arrays.asList(
+                new Dog("BibaOne", 12),
+                new Dog("BibaTwo", 10),
+                new Dog("One", 7),
+                new Dog("Two", 7),
+                new Dog("Three", 3)
+        );
+        Predicate<Dog> dogPredicate = new DogPredicate();
+
+        List<Dog> filteredList = filterDogs(dogList, dogPredicate);
+        System.out.println(filteredList);
+
+        }
+
+
+        public static <T>List<T> filterDogs(List<T> dogList, Predicate<T> dogPredicate) {
+            List<T> result = new ArrayList<>();
+
+            for (T t: dogList) {
+                if (dogPredicate.test(t)) {
+                    result.add(t);
+                }
+            }
+            return result;
     }
 }
