d08b6b885e6e6f4549f146351671b7dafee7d472
functional_interface <defied_jeff@mail.ru>
2023-03-25 00:17:56 +1200

    Вроде бы все

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача №1");
+        System.out.println("Для себя");
         List<Dog> dogList = Arrays.asList(
                 new Dog("BibaOne", 12),
                 new Dog("BibaTwo", 10),
@@ -15,20 +18,74 @@ public class Main {
         );
         Predicate<Dog> dogPredicate = new DogPredicate();
 
-        List<Dog> filteredList = filterDogs(dogList, dogPredicate);
-        System.out.println(filteredList);
-
-        }
 
 
-        public static <T>List<T> filterDogs(List<T> dogList, Predicate<T> dogPredicate) {
-            List<T> result = new ArrayList<>();
-
-            for (T t: dogList) {
-                if (dogPredicate.test(t)) {
-                    result.add(t);
-                }
+        System.out.println("Задание №1");
+        Predicate<Integer> predicate = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer > 0;
             }
-            return result;
+        };
+
+        Predicate<Integer> predicate1 = integer -> {
+            return integer > 0;
+        };
+        System.out.println(predicate.test(-1));
+        System.out.println(predicate1.test(12));
+        System.out.println();
+
+        System.out.println("Задание №2");
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("Привет, " + s);
+            }
+        };
+        consumer.accept("Олег");
+
+        Consumer<String> consumer1 = s -> {
+            System.out.println("Привет, " + s);
+        };
+
+        consumer1.accept("Шоня");
+        System.out.println();
+
+        System.out.println("Задание №3");
+       Function<Double, Long> function = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return Math.round(aDouble);
+            }
+        };
+
+        Function<Double, Long> function1 = aDouble -> {
+            return Math.round(aDouble);
+        };
+
+        System.out.println(function.apply(2.35689515465464));
+        System.out.println(function1.apply(5.3698446546543241));
+        System.out.println();
+
+        System.out.println("Задание №4");
+
+        Supplier<Integer> supplier = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return (int) (Math.random() * 100);
+            }
+        };
+
+        Supplier<Integer> supplier1 = () -> (int) (Math.random() * 100);
+
+        System.out.println(supplier.get());
+        System.out.println(supplier1.get());
+        System.out.println();
     }
 }
+
+
+
+
+
+
